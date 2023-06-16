249d0fdd9d114608cde921921835a61a8d785ca2
Gedion Sang <sangkybet4@gmail.com>
2023-06-16 13:10:02 -0700

    ALPHabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,9 +9,11 @@ int main(void)
 {
 	char i;
 
-	for (i = 'f'; i <= 'k'; i++)
-	{
-		putchar(i);
+	i = 'f';
+        while
+       	       (i <= 'k');
+			putchar(i)
+			i++;
 	}
 	putchar("\n");
 	return (0);
