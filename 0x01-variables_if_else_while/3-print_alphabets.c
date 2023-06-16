4f520010ace702a764959c0dea5fb22dc528a91b
Gedion Sang <sangkybet4@gmail.com>
2023-06-16 12:40:08 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - Using printf and puts functions is forbidden
+ *Return:0
+ */
+
+int main(void)
+{
+	char a;
+	char b;
+
+	for (a = 'f'; a < = 'k'; a++)
+	{
+		putchar(i);
+	}
+	for (b = 'F'; b < = 'K'; b++)
+	{
+		putchar(b);
+	}
+	putchar("\n");
+	return (0);
+}
