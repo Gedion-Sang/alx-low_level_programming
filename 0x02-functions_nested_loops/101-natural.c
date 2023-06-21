8ad8da518d9514478c6eb62fecbd6663484c7610
Gedion Sang <sangkybet4@gmail.com>
2023-06-21 15:48:35 -0700

    Natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,14 +8,12 @@ int main(void)
 {
 	int a, b;
 
-	while (a < 1024)
+	for (a = 1; a < 1024; a++;)
 	{
-		if ((a % 3 == 0) || (b % 5 == 0))
-		{
+		if ((a % 3 == 0) || (a % 5 == 0))
 			b += a;
-		}
-		a++;
 	}
+
 	printf("%d\n", b);
 	return (0);
 }
