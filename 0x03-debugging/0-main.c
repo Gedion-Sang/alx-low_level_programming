db7df6f570e1ca991b2ce39207f91e0f102595fc
Gedion Sang <sangkybet4@gmail.com>
2023-06-22 03:12:14 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-#include"main"
 /**
  * main - main block
  * Description: Get a random number and print the number
@@ -16,7 +15,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%i is positive\n", n);
-	else if (n < 0)
+	else if (n == 0)
 		printf("%i is negative\n", n);
 	else
 		printf("%i is zero\n", n);
